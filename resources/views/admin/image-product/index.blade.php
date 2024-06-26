21076adf2a8ea9595cf0fd9bfca6c3415a3d83e7
My bee <111343400+Dame-killer@users.noreply.github.com>
2024-06-26 15:13:13 +0700

    image áo polo

--- resources/views/admin/image-product/index.blade.php
+++ resources/views/admin/image-product/index.blade.php
@@ -86,6 +86,9 @@
                                     @endforeach
                                 </tbody>
                             </table>
+                            {{-- <div class="d-flex justify-content-center mt-3">
+                                {{ $brands->appends(request()->query())->links() }}
+                            </div> --}}
                         </div>
                     </div>
                 </div>
