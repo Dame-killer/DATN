9803118ac5672f3733da3acd7ee33141807673a6
My bee <111343400+Dame-killer@users.noreply.github.com>
2024-07-01 10:56:39 +0700

    .

--- resources/views/admin/order/invoice.blade.php
+++ resources/views/admin/order/invoice.blade.php
@@ -1,158 +1,149 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hóa Đơn</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <style>
-        body {
-            background: #f2f2f2;
-            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-        }
-        .invoice-box {
-            max-width: 800px;
-            margin: 50px auto;
-            padding: 30px;
-            border: 1px solid #eee;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-            background: #fff;
-            color: #555;
-        }
-        .invoice-box table {
-            width: 100%;
-            line-height: inherit;
-            text-align: left;
-            border-collapse: collapse;
-        }
-        .invoice-box table td {
-            padding: 10px;
-            vertical-align: top;
-        }
-        .invoice-box table tr td:nth-child(2),
-        .invoice-box table tr td:nth-child(4),
-        .invoice-box table tr td:nth-child(6) {
-            text-align: right;
-        }
-        .invoice-box table tr.top table td {
-            padding-bottom: 20px;
-        }
-        .invoice-box table tr.top table td.title h1 {
-            font-size: 45px;
-            line-height: 45px;
-            color: #333;
-        }
-        .invoice-box table tr.information table td {
-            padding-bottom: 40px;
-        }
-        .invoice-box table tr.heading td {
-            background: #eee;
-            border-bottom: 1px solid #ddd;
-            font-weight: bold;
-            padding: 10px;
-        }
-        .invoice-box table tr.details td {
-            padding-bottom: 20px;
-        }
-        .invoice-box table tr.item td {
-            border-bottom: 1px solid #eee;
-        }
-        .invoice-box table tr.item.last td {
-            border-bottom: none;
-        }
-        .invoice-box table tr.total td:nth-child(2) {
-            border-top: 2px solid #eee;
-            font-weight: bold;
-            text-align: right;
-            padding-top: 20px;
-            font-size: 18px;
-        }
-        .title h1 {
-            font-size: 36px;
-            color: #333;
-        }
-        .information td, .total td {
-            font-weight: bold;
-        }
-        .invoice-box table tr.item td {
-            padding: 15px 0;
-        }
-        .invoice-box table tr.total td {
-            font-size: 20px;
-        }
-    </style>
 </head>
+
+<style>
+    .invoice-box {
+        max-width: 800px;
+        margin: auto;
+        padding: 30px;
+        border: 1px solid #eee;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+        font-size: 16px;
+        line-height: 1.6;
+        font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        color: #555;
+    }
+
+    .invoice-box h1 {
+        font-size: 36px;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .invoice-box .order-info {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+
+    .invoice-box .order-info .order-date,
+    .invoice-box .order-info .order-code {
+        width: 45%;
+    }
+
+    .invoice-box .information {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+
+    .invoice-box .information .receiver-info,
+    .invoice-box .information .customer-account {
+        width: 45%;
+    }
+
+    .invoice-box table {
+        width: 100%;
+        line-height: inherit;
+        text-align: left;
+        border-collapse: collapse;
+    }
+
+    .invoice-box table td {
+        padding: 5px;
+        vertical-align: top;
+    }
+
+    .invoice-box table tr.heading td {
+        background: #eee;
+        border-bottom: 1px solid #ddd;
+        font-weight: bold;
+    }
+
+    .invoice-box table tr.item td {
+        border-bottom: 1px solid #eee;
+    }
+
+    .invoice-box table tr.total td:nth-child(2) {
+        border-top: 2px solid #eee;
+        font-weight: bold;
+    }
+
+    .label {
+        font-weight: bold;
+    }
+</style>
+
 <body>
-<div class="invoice-box">
-    <table cellpadding="0" cellspacing="0">
-        <tr class="top">
-            <td colspan="2">
-                <table>
-                    <tr>
-                        <td class="title">
-                            <h1>Hóa Đơn</h1>
-                        </td>
-                        <td>
-                            Mã Đơn Hàng #: {{ $orders->code }}<br>
-                            Ngày Đặt Hàng: {{ $orders->order_date }}<br>
-                        </td>
+    <div class="invoice-box">
+        <div class="top">
+            <h1>Hóa Đơn</h1>
+            <div class="order-info">
+                <div class="order-date">
+                    <span class="label">Ngày Đặt Hàng:</span> {{ $orders->order_date }}
+                </div>
+                <div class="order-code">
+                    <span class="label">Mã Đơn Hàng:</span> {{ $orders->code }}
+                </div>
+            </div>
+        </div>
+        <div class="information">
+            <div class="receiver-info">
+                <div>Người Nhận: {{ $orders->receiver }}</div>
+                <div>Địa chỉ: {{ $orders->address }}</div>
+                <div>Số điện thoại: {{ $orders->phone }}</div>
+            </div>
+            <div class="customer-account">
+                <div>Tài Khoản Khách Hàng: {{ $orders->user ? $orders->user->email : 'N/A' }}</div>
+            </div>
+        </div>
+        <div class="payment-info">
+            <div>Phương Thức Thanh Toán: {{ $orders->paymentMethod->name }}</div>
+            <div>Mã Vận Đơn: {{ $orders->tracking_code }}</div>
+        </div>
+        <div class="information">
+            <table class="m-2">
+                <tr class="heading">
+                    <td>Sản Phẩm</td>
+                    <td>Kích Cỡ</td>
+                    <td>Màu Sắc</td>
+                    <td>Giá</td>
+                    <td>Số Lượng</td>
+                    <td>Tổng</td>
+                </tr>
+                @foreach ($orderDetails as $orderDetail)
+                    <tr class="item">
+                        <td>{{ $orderDetail->productDetail->product->code }} -
+                            {{ $orderDetail->productDetail->product->name }}</td>
+                        <td>{{ $orderDetail->productDetail->size->size_name }} -
+                            {{ $orderDetail->productDetail->size->size_number }}</td>
+                        <td>{{ $orderDetail->productDetail->color->name }}</td>
+                        <td>{{ number_format($orderDetail->unit_price) }} VNĐ</td>
+                        <td>{{ $orderDetail->amount }}</td>
+                        <td>{{ number_format($orderDetail->totalPricePerProduct) }} VNĐ</td>
                     </tr>
-                </table>
-            </td>
-        </tr>
-        <tr class="information">
-            <td colspan="2">
-                <table>
-                    <tr>
-                        <td>
-                            Người Nhận:<br>
-                            {{ $orders->receiver }}<br>
-                            {{ $orders->address }}<br>
-                            {{ $orders->phone }}
-                        </td>
-                        <td>
-                            Tài Khoản Khách Hàng:<br>
-                            {{ $orders->user ? $orders->user->email : 'N/A' }}
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-        <tr class="heading">
-            <td>Phương Thức Thanh Toán</td>
-            <td>{{ $orders->paymentMethod->name }}</td>
-        </tr>
-        <tr class="heading">
-            <td>Mã Vận Đơn</td>
-            <td>{{ $orders->tracking_code }}</td>
-        </tr>
-        <tr class="heading">
-            <td>Sản Phẩm</td>
-            <td>Kích Cỡ</td>
-            <td>Màu Sắc</td>
-            <td>Giá</td>
-            <td>Số Lượng</td>
-            <td>Tổng</td>
-        </tr>
-        @foreach ($orderDetails as $orderDetail)
-            <tr class="item">
-                <td>{{ $orderDetail->productDetail->product->code }} - {{ $orderDetail->productDetail->product->name }}</td>
-                <td>{{ $orderDetail->productDetail->size->size_name }} - {{ $orderDetail->productDetail->size->size_number }}</td>
-                <td>{{ $orderDetail->productDetail->color->name }}</td>
-                <td>{{ number_format($orderDetail->unit_price) }} VNĐ</td>
-                <td>{{ $orderDetail->amount }}</td>
-                <td>{{ number_format($orderDetail->totalPricePerProduct) }} VNĐ</td>
-            </tr>
-        @endforeach
-        <tr class="total">
-            <td colspan="4"></td>
-            <td>Tổng Cộng</td>
-            <td>{{ number_format($totalPrice) }} VNĐ</td>
-        </tr>
-    </table>
-</div>
-<script>
-    window.onload = function() {
-        window.print();
-    };
-</script>
+                @endforeach
+                <tr class="total">
+                    <td colspan="4"></td>
+                    <td>Tổng Cộng</td>
+                    <td>{{ number_format($totalPrice) }} VNĐ</td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+
+
+    {{-- <script>
+        window.onload = function() {
+            window.print();
+        };
+    </script> --}}
 </body>
+
 </html>
--- resources/views/customer/shoping-cart.blade.php
+++ resources/views/customer/shoping-cart.blade.php
@@ -18,118 +18,85 @@
     </div>
     <!-- Shoping Cart -->
     <div class="bg0 p-t-75 p-b-85">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 ">
-                    <div class="m-l-25 m-r--38 m-lr-0-xl">
-                        <div class="wrap-table-shopping-cart">
-                            <table class="table-shopping-cart">
-                                <tr class="table_head">
-                                    <th class="column-1">STT</th>
-                                    <th class="column-2">Mã Sản Phẩm</th>
-                                    <th class="column-3">Tên Sản Phẩm</th>
-                                    <th class="column-4">Hình Ảnh</th>
-                                    <th class="column-5">Số Lượng</th>
-                                    <th class="column-6">Giá</th>
-                                    <th class="column-7">Kích Cỡ</th>
-                                    <th class="column-8">Màu sắc</th>
-                                    <th class="column-9">Tổng</th>
-                                    <th></th>
+        {{-- <div class="container"> --}}
+        <div class="row">
+            <div class="col-xl-8 col-lg-8 m-l-25 m-r--38 m-lr-0-xl">
+                <div class="wrap-table-shopping-cart">
+                    <table class="table-shopping-cart border-only">
+                        <tr class="table_head">
+                            <th class="column-0">STT</th>
+                            <th class="column-2">Sản phẩm</th>
+                            <th class="column-3">Số Lượng</th>
+                            <th class="column-3">Giá</th>
+                            <th class="column-3">Tổng</th>
+                            <th></th>
+                        </tr>
+                        <tbody>
+                            @foreach ($order_details as $order_detail)
+                                <tr>
+                                    <td>
+                                        {{-- <div class="text-x justify-content-center" style="hight-max: auto"> --}}
+                                        <h6 class="mb-0 text-sm justify-content-center">{{ $loop->iteration }}</h6>
+                                        {{-- </div> --}}
+                                    </td>
+                                    <td>
+                                        <p class="text-xs font-weight-bold mb-0">
+                                            <img src="{{ $order_detail->product_detail->product->image }}"
+                                                alt="{{ $order_detail->product_detail->product->name }}" class="img-fluid"
+                                                style="width: 50px; height: 50px;"><br>
+                                            {{ $order_detail->product_detail->product->name }}/{{ $order_detail->product_detail->product->code }}/
+                                            {{ $order_detail->product_detail->size->size_name }}
+                                            -{{ $order_detail->product_detail->size->size_number }}/{{ $order_detail->product_detail->color->name }}
+                                        </p>
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <button class="btn btn-sm btn-warning update-quantity"
+                                                data-id="{{ $order_detail->product_detail->id }}"
+                                                data-action="decrease">-</button>
+                                            <p class="text-xs font-weight-bold mb-0 mx-2">{{ $order_detail->amount }}
+                                            </p>
+                                            <button class="btn btn-sm btn-primary update-quantity"
+                                                data-id="{{ $order_detail->product_detail->id }}"
+                                                data-action="increase">+</button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <p class="text-right font-weight-bold mb-0">{{ $order_detail->unit_price }}đ</p>
+                                    </td>
+                                    <td>
+                                        <p class="text-right font-weight-bold mb-0 total-price-per-product">
+                                            {{ $order_detail->totalPricePerProduct }}đ</p>
+                                    </td>
+                                    <td class="text-right">
+                                        <form
+                                            action="{{ route('customer-cart-remove', $order_detail->product_detail->id) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm">X</button>
+                                        </form>
+                                    </td>
                                 </tr>
-                                <tbody>
-                                    @foreach ($order_details as $order_detail)
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex ">
-                                                    <div class="d-flex flex-column justify-content-center">
-                                                        <h6 class="mb-0 text-sm">{{ $loop->iteration }}</h6>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">
-                                                    {{ $order_detail->product_detail->product->code }}</p>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">
-                                                    {{ $order_detail->product_detail->product->name }}</p>
-                                            </td>
-                                            <td class="text-center">
-                                                <img src="{{ $order_detail->product_detail->product->image }}"
-                                                    alt="{{ $order_detail->product_detail->product->name }}"
-                                                    class="img-fluid" style="width: 50px; height: 50px;">
-                                            </td>
-                                            <td>
-                                                <div class="d-flex align-items-center">
-                                                    <button class="btn btn-sm btn-warning update-quantity"
-                                                        data-id="{{ $order_detail->product_detail->id }}"
-                                                        data-action="decrease">-
-                                                    </button>
-                                                    <p class="text-xs font-weight-bold mb-0 mx-2">
-                                                        {{ $order_detail->amount }}
-                                                    </p>
-                                                    <button class="btn btn-sm btn-primary update-quantity"
-                                                        data-id="{{ $order_detail->product_detail->id }}"
-                                                        data-action="increase">+
-                                                    </button>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">{{ $order_detail->unit_price }}đ
-                                                </p>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">
-                                                    {{ $order_detail->product_detail->size->size_name }}
-                                                    -{{ $order_detail->product_detail->size->size_number }}</p>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">
-                                                    {{ $order_detail->product_detail->color->name }}</p>
-                                                <div
-                                                    style="width: 20px; height: 20px; background-color: {{ $order_detail->product_detail->color->code }};">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0 total-price-per-product">
-                                                    {{ $order_detail->totalPricePerProduct }}
-                                                    đ</p>
-                                            </td>
-                                            <td class="text-right">
-                                                <form
-                                                    action="{{ route('customer-cart-remove', $order_detail->product_detail->id) }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm">Xóa</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-
-                    </div>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            <div class="col-sm-10 col-lg-7 col-xl-5 ms-auto me-2 m-b-50">
+            <div class="col-xl-4 col-lg-4 ms-auto me-2 m-b-50 ">
                 <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
-
                     <div class="flex-w flex-t bor12 p-b-13">
                         <div class="size-208">
                             <span class="stext-110 cl2">
                                 Tổng cộng
                             </span>
                         </div>
-
                         <div class="size-209">
                             <span class="mtext-110 cl2" id="total-price">
                                 {{ $totalPrice }}đ
                             </span>
                         </div>
                     </div>
-
                     <button type="button"
                         class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer"
                         data-bs-toggle="modal" data-bs-target="#addOrderModal">
@@ -138,6 +105,7 @@
                 </div>
             </div>
         </div>
+        {{-- </div> --}}
     </div>
 
     <div class="modal fade" id="addOrderModal" tabindex="-1" aria-labelledby="addOrderModalLabel" aria-hidden="true">
@@ -244,6 +212,7 @@
         /* Bootstrap default z-index for modals is usually around this value */
     }
 
+
     .bread-crumb a.stext-109,
     .bread-crumb span.stext-109 {
         font-size: 18px;
@@ -265,19 +234,20 @@
         border-collapse: collapse;
         margin: 20px 0;
         font-size: 16px;
-        text-align: left;
+        text-align: center;
     }
 
     .table-shopping-cart th,
     .table-shopping-cart td {
         padding: 12px 15px;
-        border: 1px solid #ddd;
+        border: none;
     }
 
     .table-shopping-cart thead th {
         background-color: #f8f9fa;
         color: #333;
         font-weight: bold;
+        border-bottom: 2px solid black;
     }
 
     .table-shopping-cart tbody tr {
