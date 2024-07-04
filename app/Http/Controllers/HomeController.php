018b8299e357c1d6e5fa09c855f0044589909725
ThanhLongg0412 <longgsatou@gmail.com>
2024-07-04 15:48:44 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -116,49 +116,49 @@ class HomeController extends Controller
     }
 
     public function homeCustomer()
-{
-    $products = Product::all();
-    $categories = Category::all();
-    $brands = Brand::all();
-    $totalOrders = $this->getTotalOrders();
-    $cart = session()->get('cart', []); // Lấy nội dung giỏ hàng từ session
+    {
+        $products = Product::all();
+        $categories = Category::all();
+        $brands = Brand::all();
+        $totalOrders = $this->getTotalOrders();
+        $cart = session()->get('cart', []); // Lấy nội dung giỏ hàng từ session
 
-    $order_details = [];
-    $totalPrice = 0;
+        $order_details = [];
+        $totalPrice = 0;
 
-    foreach ($cart as $item) {
-        // Tạo một đối tượng OrderDetail tạm thời để hiển thị trên view
-        $order_detail = new \stdClass();
-        $order_detail->product_detail = new \stdClass();
-        $order_detail->product_detail->product = new \stdClass();
-        $order_detail->product_detail->color = new \stdClass();
-        $order_detail->product_detail->size = new \stdClass();
+        foreach ($cart as $item) {
+            // Tạo một đối tượng OrderDetail tạm thời để hiển thị trên view
+            $order_detail = new \stdClass();
+            $order_detail->product_detail = new \stdClass();
+            $order_detail->product_detail->product = new \stdClass();
+            $order_detail->product_detail->color = new \stdClass();
+            $order_detail->product_detail->size = new \stdClass();
 
-        $order_detail->product_detail->id = $item['id'];
-        $order_detail->product_detail->product->code = $item['attributes']['product_code'];
-        $order_detail->product_detail->product->name = $item['name'];
-        $order_detail->product_detail->product->image = $item['attributes']['product_image'];
-        $order_detail->amount = $item['quantity'];
-        $order_detail->unit_price = $item['attributes']['product_price'];
-        $order_detail->product_detail->size->size_name = $item['attributes']['size_name'];
-        $order_detail->product_detail->size->size_number = $item['attributes']['size_number'];
-        $order_detail->product_detail->color->name = $item['attributes']['color_name'];
-        $order_detail->product_detail->color->code = $item['attributes']['color_code'];
-        $order_detail->totalPricePerProduct = $order_detail->unit_price * $order_detail->amount;
+            $order_detail->product_detail->id = $item['id'];
+            $order_detail->product_detail->product->code = $item['attributes']['product_code'];
+            $order_detail->product_detail->product->name = $item['name'];
+            $order_detail->product_detail->product->image = $item['attributes']['product_image'];
+            $order_detail->amount = $item['quantity'];
+            $order_detail->unit_price = $item['attributes']['product_price'];
+            $order_detail->product_detail->size->size_name = $item['attributes']['size_name'];
+            $order_detail->product_detail->size->size_number = $item['attributes']['size_number'];
+            $order_detail->product_detail->color->name = $item['attributes']['color_name'];
+            $order_detail->product_detail->color->code = $item['attributes']['color_code'];
+            $order_detail->totalPricePerProduct = $order_detail->unit_price * $order_detail->amount;
 
-        $order_details[] = $order_detail;
-        $totalPrice += $order_detail->totalPricePerProduct;
+            $order_details[] = $order_detail;
+            $totalPrice += $order_detail->totalPricePerProduct;
+        }
+
+        // Trả về view với các biến dữ liệu được compact lại
+        return view('customer.home', compact('totalOrders', 'products', 'categories', 'brands', 'order_details', 'totalPrice'));
     }
-
-    // Trả về view với các biến dữ liệu được compact lại
-    return view('customer.home', compact('totalOrders', 'products', 'categories', 'brands', 'order_details', 'totalPrice'));
-}
-
 
     public function acount()
     {
         return redirect()->route('acount');
     }
+
     public function getTotalOrders()
     {
         $totalOrders = 0;
@@ -176,5 +176,17 @@ class HomeController extends Controller
         }
 
         return $totalOrders;
+    }
+
+    public function setFlashMessage(Request $request)
+    {
+        $request->validate([
+            'message' => 'required|string',
+            'type' => 'required|string|in:success,error',
+        ]);
+
+        session()->flash($request->type, $request->message);
+
+        return response()->json(['status' => 'Message set'], 200);
     }
 }
