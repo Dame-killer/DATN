4a68e02ad11b254238885b64b02455b3a84e482a
ThanhLongg0412 <longgsatou@gmail.com>
2024-07-10 11:03:01 +0700

    .

---
+++ app/Console/Commands/CancelUnpaidOrders.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CancelUnpaidOrders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'orders:cancel-unpaid';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Đơn hàng sẽ bị hủy sau 7 ngày nếu không thanh toán!';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Lấy ngày hiện tại
+        $now = Carbon::now();
+
+        // Lấy các đơn hàng chưa thanh toán và đã quá 7 ngày
+        $orders = Order::where('payment_status', 0)
+            ->where('status', '<', 3)
+            ->where('order_date', '<', $now->subDays(7))
+            ->get();
+
+        foreach ($orders as $order) {
+            $order->status = 4; // 4 là trạng thái hủy đơn
+            $order->save();
+        }
+
+        $this->info('Đơn hàng sẽ bị hủy sau 7 ngày nếu không thanh toán!');
+    }
+}
