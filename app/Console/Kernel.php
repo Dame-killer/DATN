7e32c1140453814b18eae408066dafe872caea49
ThanhLongg0412 <longgsatou@gmail.com>
2024-07-10 11:03:21 +0700

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('orders:cancel-unpaid')->everyMinute();
+        $schedule->command('orders:cancel-unpaid')->daily();
     }
 
     /**
